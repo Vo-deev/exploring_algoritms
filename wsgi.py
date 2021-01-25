34304d02f791e8c96a987e9e224c87a9f9f7fd5a
Vo-deev <box4sickspam@gmail.com>
2021-01-25 17:30:34 +0200

    1st form done

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,32 @@
-from flask import Flask
-from flask import app
-from flask import render_template
+import os
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+
+SECRET_KEY = os.urandom(43)#Создаем рандомнй ключ
+app.config['SECRET_KEY'] = SECRET_KEY
+
+
+class MyForm(FlaskForm):
+    name = StringField('name')
 
 
 @app.route('/')
-def main():
+def hello_world():
     return render_template('main.html')
+
+
+@app.route('/sign/', methods=['GET', 'POST'])
+def myform():
+    form = MyForm()
+    return render_template('signup.html', form=form)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
